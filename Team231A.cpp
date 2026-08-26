ac3ba245acb5d3b8fb9d6cd8d43931dcbba6e32f
Kshitij-4832 <kshitijgupta4832@gmail.com>
2026-08-26 18:51:41 +0530

    commit

---
+++ Team231A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, ans = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        int p, v, t;
+        cin >> p >> v >> t;
+        if (p + v + t >= 2)
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
