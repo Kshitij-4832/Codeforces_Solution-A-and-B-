8fda59fb7a6a20f09f6f19356a4870497b519f95
Kshitij-4832 <kshitijgupta4832@gmail.com>
2026-09-02 14:04:37 +0530

    commit

---
+++ Expression479A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    int p = a + (b * c), q = a * b * c, r = a * (b + c), s = (a + b) * c, t = a + b + c;
+    cout << max({p, q, r, s, t}) << endl;
+    return 0;
+}
