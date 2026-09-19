847ad58885b16b455b635a1abbb940169a72a1a8
Kshitij-4832 <kshitijgupta4832@gmail.com>
2026-09-19 18:48:52 +0530

    commit

--- Nearest_Interesting_Number1183A.cpp
+++ Nearest_Interesting_Number1183A.cpp
@@ -1,43 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-int sumofdigit(int n)
-{
-    int ans = 0;
-    while (n != 0)
-    {
-        ans = ans + (n % 10);
-        n = n / 10;
-    }
-    return ans;
-}
 int main()
 {
     int a = 0;
     cin >> a;
-    int p = sumofdigit(a);
-    int q = sumofdigit(a + 1);
-    int r = sumofdigit(a + 2);
-    int s = sumofdigit(a + 3);
-    int t = sumofdigit(a + 4);
-    if (p % 4 == 0)
+    while (true)
     {
-        cout << a << endl;
-    }
-    else if (q % 4 == 0)
-    {
-        cout << a + 1 << endl;
-    }
-    else if (r % 4 == 0)
-    {
-        cout << a + 2 << endl;
-    }
-    else if (s % 4 == 0)
-    {
-        cout << a + 3 << endl;
-    }
-    else
-    {
-        cout << a + 4 << endl;
+        int copy = a, ans = 0;
+        while (copy != 0)
+        {
+            ans = ans + (copy % 10);
+            copy = copy / 10;
+        }
+        if (ans % 4 == 0)
+        {
+            cout << a << endl;
+            break;
+        }
+        a++;
     }
     return 0;
 }
